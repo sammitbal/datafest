b992d13da0e5956ca9f13084e40a92c6c3a9eca9
sammitbal <73960115+sammitbal@users.noreply.github.com>
2025-04-11 18:49:30 -0400

    Update datafestRscript.R

---
+++ datafestRscript.R
@@ -0,0 +1,8 @@
+
+#Front Matter 
+
+MajorMarketOccupancy <- read.csv("./Major Market Occupancy Data-revised.csv")
+
+PriceandAvailability <- read.csv("./Price and Availability Data.csv")
+
+Unemployment <- read.csv("./Unemployment.csv")
